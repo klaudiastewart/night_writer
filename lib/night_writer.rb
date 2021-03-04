91a1db4e11b54e20a789aea088a023e418d90abd
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-04 15:57:55 -0700

    Try out File.open

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,6 +2,26 @@
 
 # puts 'hello world'
 
-File.open("braille.txt", "w+") {|f| f.write("strangeworld") }
 
-# puts "Created "" containing 256 characters" #use string interpolation
+class Writer
+
+  def initialize()
+  end
+
+  def start
+    puts "Welcome to night writer, how many words do you want to read?"
+    user_input = gets.chomp
+    puts user_input
+    if user_input == Integer
+      puts 'hi'
+    end
+  end
+end
+
+writer = Writer.new
+writer.start
+
+# user_input = gets.chomp
+new_file = File.open("braille.txt", "w+") {|f| f.write("yoooo") } #For f.write argument,
+# do I need to make a method to say whatever the size is, make a random word from that?
+puts "Created 'braille.txt' containing characters"
