017bf3676de33dca88f0943d29ba220286c98b9e
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-05 15:23:59 -0700

    Refactor file open for start method

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -11,22 +11,54 @@ class NightWriter
     @output_file = output_file
     @input = ARGV[0]
     @input_2 = ARGV[1]
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
   end
 
   def start
-    # input = ARGV[0]
-    # input_2 = ARGV[1]
-    # require "pry"; binding.pry
-    new_file = File.open("braille.txt", "w") do |fo|
-      fo.puts "#{@input_2}"
+    File.open("message.txt", "r+") do |f|
+      f.puts "#{@input_2}"
     end
+    File.open("braille.txt", "w") {|fo| fo.write("#{@input_2}") }
     puts "Created '#{output_file}' containing #{@input_2.length} characters"
+    convert
   end
 
-  def convert(letter)
+  def convert
+    english_letter_to_braille_symbol = {
+      "a" => "0.....",
+      "b" => "0.0...",
+      "c" => "00....",
+      "d" => "00.0..",
+      "e" => "0..0..",
+      "f" => "000...",
+      "g" => "0000..",
+      "h" => "0.00..",
+      "i" => ".00...",
+      "j" => ".000..",
+      "k" => "0...0.",
+      "l" => "0.0.0.",
+      "m" => "00..0.",
+      "n" => "00.00.",
+      "o" => "0..00.",
+      "p" => "000.0..",
+      "q" => "00000.",
+      "r" => "0.000.",
+      "s" => ".00.0.",
+      "t" => ".0000.",
+      "u" => "0...00",
+      "v" => "0.0.00",
+      "w" => ".000.0",
+      "x" => "00..00",
+      "y" => "00.000",
+      "z" => "0..000"
+    }
     # require "pry"; binding.pry
   end
+
+  def conversion
+
+  end
+
 end
 
 
