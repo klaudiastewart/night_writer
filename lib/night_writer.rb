18b9fb2c7f1f8757d7af51283c4f953853e8fbbc
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-04 18:07:17 -0700

    Test start method functionality

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,27 +1,25 @@
 # require './message.txt'
 
-# puts 'hello world'
+class NightWriter
 
-
-class Writer
-
-  def initialize()
+  def initialize(length = nil, file_name = nil)
+    @length = length
+    @file_name = file_name
   end
 
   def start
     puts "Welcome to night writer, how many words do you want to read?"
     user_input = gets.chomp
-    puts user_input
-    if user_input == Integer
-      puts 'hi'
-    end
+    new_file = File.open("braille.txt", "w+")
+    puts "Created #{new_file} containing #{user_input} characters"
   end
 end
 
-writer = Writer.new
-writer.start
-
 # user_input = gets.chomp
-new_file = File.open("braille.txt", "w+") {|f| f.write("yoooo") } #For f.write argument,
+# new_file = File.open(file_name, "w+")
+#{|f| f.write("yoooo") } #For f.write argument,
 # do I need to make a method to say whatever the size is, make a random word from that?
-puts "Created 'braille.txt' containing characters"
+# puts "Created #{file_name} containing characters"
+
+writer = NightWriter.new()
+writer.start
