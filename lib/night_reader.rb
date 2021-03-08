4bd99b5d3519987564170934c988a807e21a8651
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-07 17:39:13 -0700

    Complete dynamic conversion method for less than eighty characters

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -17,40 +17,44 @@ class NightReader
 
   def convert
     braille_to_english_letter = {
-      ["0.", "..", ".."]=>"a",
-      ["0.", "0.", ".."]=>"b",
-      ["00", "..", ".."]=>"c",
-      ["00", ".0", ".."]=>"d",
-      ["0.", ".0", ".."]=>"e",
-      ["00", "0.", ".."]=>"f",
-      ["00", "00", ".."]=>"g",
+      "0....."=>"a",
+      "0.0..."=>"b",
+      "00...."=>"c",
+      "00.0.."=>"d",
+      "0..0.."=>"e",
+      "000..."=>"f",
+      "0000.."=>"g",
       "0.00.."=>"h",
-      [".0", "0.", ".."]=>"i",
-      [".0", "00", ".."]=>"j",
-      ["0.", "..", "0."]=>"k",
-      ["0.", "0.", "0."]=>"l",
-      ["00", "..", "0."]=>"m",
-      ["00", ".0", "0."]=>"n",
-      ["0.", ".0", "0."]=>"o",
-      ["00", "0.", "0."]=>"p",
-      ["00", "00", "0."]=>"q",
-      ["0.", "00", "0."]=>"r",
-      [".0", "0.", "0."]=>"s",
-      [".0", "00", "0."]=>"t",
-      ["0.", "..", "00"]=>"u",
-      ["0.", "0.", "00"]=>"v",
-      [".0", "00", ".0"]=>"w",
-      ["00", "..", "00"]=>"x",
-      ["00", ".0", "00"]=>"y",
-      ["0.", ".0", "00"]=>"z",
-      ["..", "..", ".."]=>" ",
-      ["..", "00", ".0"]=>"."
+      ".00..."=>"i",
+      ".000.."=>"j",
+      "0...0."=>"k",
+      "0.0.0."=>"l",
+      "00..0."=>"m",
+      "00.00."=>"n",
+      "0..00."=>"o",
+      "000.0."=>"p",
+      "00000."=>"q",
+      "0.000."=>"r",
+      ".00.0."=>"s",
+      ".0000."=>"t",
+      "0...00"=>"u",
+      "0.0.00"=>"v",
+      ".000.0"=>"w",
+      "00..00"=>"x",
+      "00.000"=>"y",
+      "0..000"=>"z",
+      "......"=>" ",
+      "..00.0"=>"."
     }
   end
 
   def read_file
     read = File.read("#{@input_file}").chomp
-    no_line = read.delete("\n")
+    # no_line = read.delete("\n")
+    read.split
+    read.split.map do |arr|
+      arr.scan(/../)
+    end
     # read == message on night_writer
 
     # if read.length > 80
@@ -58,27 +62,28 @@ class NightReader
     #   x = array.map do |letter|
     #     letter.chars
     #   end
-    array = no_line.chars
-    count = array.count / 3
-    product = array.each_slice(count).to_a #makes an array of three arrays each representing top, middle, bottom
-
+    # array = no_line.chars
+    # count = array.count / 3
+    # product = array.each_slice(count).to_a #makes an array of three arrays each representing top, middle, bottom
+    # require "pry"; binding.pry
   end
 
   def conversion
-      top = ""
-      middle = ""
-      bottom = ""
-
-      letters = read_file.each do |braille|
-        top << braille[0..1].join
-        middle << braille[2..3].join
-        bottom << braille[4..5].join
+    stringed_message = ""
+    count = 0
+    until count == read_file[0].size do
+      read_file.each do |braille|
+        stringed_message << braille[count]
       end
-      letters = convert[top] + convert[middle] + convert[bottom]
-      letters
+      count += 1
     end
-
-    def show_change_in_output_file
-      File.open("#{@output_file}", "w") {|fo| fo.write("#{conversion}") }
-    end
+    output_message = stringed_message.scan(/....../).map do |string|
+      convert[string]
+    end.join
+    output_message
   end
+
+  def show_change_in_output_file
+    File.open("#{@output_file}", "w") {|fo| fo.write("#{conversion}") }
+  end
+end
